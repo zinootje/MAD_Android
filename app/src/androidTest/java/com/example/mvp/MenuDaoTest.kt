74c644ebda77d0562e370c9c47e465bd6e091e60
matthias vanpoecke <matthias.vanpoecke@student.hogent.be>
2023-12-25 23:35:35 +0100

    added MenuDaoTest

---
+++ app/src/androidTest/java/com/example/mvp/MenuDaoTest.kt
@@ -0,0 +1,75 @@
+package com.example.mvp
+
+import android.content.Context
+import androidx.room.Room
+import androidx.test.core.app.ApplicationProvider
+import androidx.test.ext.junit.runners.AndroidJUnit4
+import com.example.mvp.data.database.MenuDao
+import com.example.mvp.data.database.MenuDatabase
+import com.example.mvp.data.database.toDbMenu
+import com.example.mvp.fake.FakeDataSource
+import com.example.mvp.model.Menu
+import kotlinx.coroutines.flow.first
+import kotlinx.coroutines.runBlocking
+import org.junit.After
+import org.junit.Before
+import org.junit.Test
+import org.junit.runner.RunWith
+import java.io.IOException
+
+@RunWith(AndroidJUnit4::class)
+class MenuDaoTest {
+    private lateinit var menuDao: MenuDao
+    private lateinit var database: MenuDatabase
+
+    private val menu1 = FakeDataSource.restoMenu.toDbMenu()
+    private val menu2 = FakeDataSource.restoMenu2.toDbMenu()
+
+    private suspend fun insertMenus() {
+        menuDao.insertMenuData(menu1)
+        menuDao.insertMenuData(menu2)
+    }
+
+    private suspend fun addOneMenu() {
+        menuDao.insertMenuData(menu1)
+    }
+
+    @Before
+    fun createDatabase() {
+        val context = ApplicationProvider.getApplicationContext<Context>()
+
+        database = Room.inMemoryDatabaseBuilder(
+            context, MenuDatabase::class.java
+        ).allowMainThreadQueries().build()
+
+        menuDao = database.menuDao()
+    }
+
+    @After
+    @Throws(IOException::class)
+    fun closeDatabase() {
+        database.close()
+    }
+
+    @Test
+    @Throws(Exception::class)
+    fun dao_insertMenuData() {
+        runBlocking {
+            addOneMenu()
+            val menu = menuDao.getMenuData(menu1.location).first()
+            assert(menu == menu1)
+        }
+    }
+
+    @Test
+    @Throws(Exception::class)
+    fun dao_getMenuData() {
+        runBlocking {
+            insertMenus()
+            val menu = menuDao.getMenuData(menu1.location).first()
+            assert(menu == menu1)
+        }
+    }
+
+
+}
--- app/src/androidTest/java/com/example/mvp/fake/FakeDataSource.kt
+++ app/src/androidTest/java/com/example/mvp/fake/FakeDataSource.kt
@@ -159,6 +159,153 @@ object FakeDataSource {
             ),
         )
     )
+    val restoMenu2 = ApiRestoMenu(
+        location = "Resto 2",
+        days = listOf(
+            Day(
+                dag = "Monday",
+                message = "Open",
+                menu = mapOf(
+                )
+            ),
+            Day(
+                dag = "Tuesday",
+                message = "",
+                menu = mapOf(
+                    "Lunch" to listOf(
+                        Dish(
+                            name = "Dish 1",
+                            special = Special.NONE
+                        ),
+                        Dish(
+                            name = "Dish 2",
+                            special = Special.VEGAN
+                        ),
+                        Dish(
+                            name = "Dish 3",
+                            special = Special.VEGGIE
+                        ),
+                    ),
+                    "Dinner" to listOf(
+                        Dish(
+                            name = "Dish 4",
+                            special = Special.NONE
+                        ),
+                        Dish(
+                            name = "Dish 5",
+                            special = Special.VEGAN
+                        ),
+                        Dish(
+                            name = "Dish 6",
+                            special = Special.VEGGIE
+                        ),
+                    )
+                )
+            ),
+            Day(
+                dag = "Wednesday",
+                message = "",
+                menu = mapOf(
+                    "Lunch" to listOf(
+                        Dish(
+                            name = "Dish 1",
+                            special = Special.NONE
+                        ),
+                        Dish(
+                            name = "Dish 2",
+                            special = Special.VEGAN
+                        ),
+                        Dish(
+                            name = "Dish 3",
+                            special = Special.VEGGIE
+                        ),
+                    ),
+                    "Dinner" to listOf(
+                        Dish(
+                            name = "Dish 4",
+                            special = Special.NONE
+                        ),
+                        Dish(
+                            name = "Dish 5",
+                            special = Special.VEGAN
+                        ),
+                        Dish(
+                            name = "Dish 6",
+                            special = Special.VEGGIE
+                        ),
+                    )
+                )
+            ),
+            Day(
+                dag = "Thursday",
+                message = "",
+                menu = mapOf(
+                    "Lunch" to listOf(
+                        Dish(
+                            name = "Dish 1",
+                            special = Special.NONE
+                        ),
+                        Dish(
+                            name = "Dish 2",
+                            special = Special.VEGAN
+                        ),
+                        Dish(
+                            name = "Dish 3",
+                            special = Special.VEGGIE
+                        ),
+                    ),
+                    "Dinner" to listOf(
+                        Dish(
+                            name = "Dish 4",
+                            special = Special.NONE
+                        ),
+                        Dish(
+                            name = "Dish 5",
+                            special = Special.VEGAN
+                        ),
+                        Dish(
+                            name = "Dish 6",
+                            special = Special.VEGGIE
+                        ),
+                    )
+                )
+            ),
+            Day(
+                dag = "Friday",
+                message = "",
+                menu = mapOf(
+                    "Lunch" to listOf(
+                        Dish(
+                            name = "Dish 1",
+                            special = Special.NONE
+                        ),
+                        Dish(
+                            name = "Dish 2",
+                            special = Special.VEGAN
+                        ),
+                        Dish(
+                            name = "Dish 3",
+                            special = Special.VEGGIE
+                        ),
+                    ),
+                    "Dinner" to listOf(
+                        Dish(
+                            name = "Dish 4",
+                            special = Special.NONE
+                        ),
+                        Dish(
+                            name = "Dish 5",
+                            special = Special.VEGAN
+                        ),
+                        Dish(
+                            name = "Dish 6",
+                            special = Special.VEGGIE
+                        ),
+                    )
+                )
+            ),
+        )
+    )
 }
 
 
